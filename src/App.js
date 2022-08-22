a0ba5f6ee487cbaae657b7093853f380cac6871b
Chris Mittell <mittell@hotmail.co.uk>
2022-08-22 11:22:07 +0900

    feat: added elements and interaction logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-	return <></>;
+	const [sourceCode, setSourceCode] = useState('');
+	const containerRef = useRef(null);
+
+	useEffect(() => {
+		containerRef.current.focus();
+
+		fetch('code.txt')
+			.then((res) => {
+				return res.text();
+			})
+			.then((text) => {
+				setSourceCode(text);
+			});
+	}, []);
+
+	const typeCode = () => {
+		console.log('typing...');
+	};
+
+	const removeMessage = () => {
+		console.log('escape!');
+	};
+
+	const keyDownHandler = (event) => {
+		if (event.key !== 'Escape') {
+			typeCode();
+		} else {
+			removeMessage();
+		}
+	};
+
+	return (
+		<>
+			<div
+				id='container'
+				onKeyDown={keyDownHandler}
+				tabIndex={0}
+				ref={containerRef}
+			>
+				<div id='source'></div>
+			</div>
+		</>
+	);
 }
 
 export default App;
